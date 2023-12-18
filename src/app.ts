c03b83bdd74d8a7b8633f352b13e888b402cb8ad
Barbara Szewczyk <comamysza@gmail.com>
2023-12-18 13:56:05 +0100

    Day 5 part 1 solution

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { solution5part1 } from './solution5.1';
+import { solution5part2 } from './solution5.2';
+import { day5input } from './inputs/day5';
+
+class App {
+	public static start() {
+		console.log(solution5part2(day5input));
+	}
+}
+
+App.start();
